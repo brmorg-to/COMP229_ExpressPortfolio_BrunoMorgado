16ffa2020b80f735391c52b8f7efab94060eea98
Bruno Morgado <brmorg@gmail.com>
2021-10-14 19:27:51 -0400

    added CRUD opperation on contact routes

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -23,22 +23,88 @@ router.get("/", (req, res, next) => {
 
 /* GET Route for displaying the Add page - CREATE operation */
 
-router.get("/add", (req, res, next) => {});
+router.get("/add", (req, res, next) => {
+  res.render("business/add", {
+    title: "Add Contacts",
+  });
+});
 
 /* POST Route for processing the Add page - CREATE operation */
 
-router.post("/add", (req, res, next) => {});
+router.post("/add", (req, res, next) => {
+  let newContact = Contact({
+    contact_name: req.body.contact_name,
+    contact_number: req.body.contact_number,
+    email: req.body.email,
+  });
+
+  Contact.create(newContact, (err, Contact) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //Refresh Business Contacts List
+      res.redirect("/contact-list");
+    }
+  });
+});
 
 /* GET Route for displaying the Edit page - UPDATE operation */
 
-router.get("/edit/:id", (req, res, next) => {});
+router.get("/edit/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  Contact.findById(id, (err, contactToEdit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //Show the Edit View
+      res.render("business/edit", {
+        title: "Edit Contact",
+        contact: contactToEdit,
+      });
+    }
+  });
+});
 
 /* POST Route for processing the Edit page - UPDATE operation */
 
-router.post("/edit/:id", (req, res, next) => {});
+router.post("/edit/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  let updatedContact = Contact({
+    _id: id,
+    contact_name: req.body.contact_name,
+    contact_number: req.body.contact_number,
+    email: req.body.email,
+  });
+
+  Contact.updateOne({ _id: id }, updatedContact, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //Refresh the Business Contact List
+      res.redirect("/contact-list");
+    }
+  });
+});
 
 /* GET to perform contact deletion - DELETE operation */
 
-router.get("/delete/:id", (req, res, next) => {});
+router.get("/delete/:id", (req, res, next) => {
+  let id = req.params.id;
+
+  Contact.remove({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //Refresh the Business Contact List
+      res.redirect("/contact-list");
+    }
+  });
+});
 
 module.exports = router;
