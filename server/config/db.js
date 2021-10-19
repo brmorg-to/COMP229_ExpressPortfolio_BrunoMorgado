0d282cd4e90c5fcadf813e2f4b8151769f8f445e
Bruno Morgado <brmorg@gmail.com>
2021-10-18 21:04:59 -0400

    changed URI. Moved database to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  URI: "mongodb://127.0.0.1/portfolio",
+  // URI: "mongodb://127.0.0.1/portfolio",
+  URI: "mongodb+srv://brmorg:FSs9MYtgeh9BPWh4@portfoliodb.maw0g.mongodb.net/portfolio?retryWrites=true&w=majority",
 };
